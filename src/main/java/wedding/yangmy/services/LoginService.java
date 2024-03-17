26835e703a774e58534c0c132042ce6ef3d306be
TnpHau <tnphuchau.2002@gmail.com>
2024-03-17 15:16:32 +0700

    update heroku

--- src/main/java/wedding/yangmy/services/LoginService.java
+++ src/main/java/wedding/yangmy/services/LoginService.java
@@ -1,5 +1,6 @@
 package wedding.yangmy.services;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import wedding.yangmy.entity.Login;
@@ -10,6 +11,16 @@ public class LoginService {
 
     @Autowired
     private LoginRepository loginRepository;
+    @PostConstruct
+    public void initDefaultAccount() {
+        Login defaultUser = loginRepository.findByUsername("my");
+        if (defaultUser == null) {
+            defaultUser = new Login();
+            defaultUser.setUsername("my");
+            defaultUser.setPassword("1");
+            loginRepository.save(defaultUser);
+        }
+    }
 
     public Login save(Login login) {
         return loginRepository.save(login);
